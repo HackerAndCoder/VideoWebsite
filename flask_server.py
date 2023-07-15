ec12aabd5a8d3d8470fc764755bf1bc7a2e3fc70
TheCrazyModder <100743043+HackerAndCoder@users.noreply.github.com>
2023-07-15 17:28:25 -0600

    Add files via upload

--- flask_server.py
+++ flask_server.py
@@ -1,25 +1,31 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+def insert_style(document):
+    with open('style.css') as f:
+        style = f.read()
+    document = document.replace('<style>', '<style>\n' + style) # insert the style into the html cause serving serparately somehow doesn't work
+    return document
+
+def get_file(path):
+    with open(path) as f:
+        return f.read()
+
+def get_styled_html(path):
+    return insert_style(get_file(path))
+
 @app.route('/')
 def index():
-    with open('index.html') as f:
-        index = f.read()
-    with open('style.css') as f:
-        style = f.read()
-    index = index.replace('<style>', '<style>\n' + style) # insert the style into the html cause serving serparately somehow doesn't work
-    return index
-
-@app.route('/style.css')
-def style():
-    with open('style.css') as f:
-        style = f.read()
-    return style
+    return get_styled_html('index.html')
 
 @app.route('/logo.png')
 def logo():
     with open('logo.png', 'rb') as f:
         return f.read()
+
+@app.route('/signup.html')
+def signup():
+    return get_styled_html('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
